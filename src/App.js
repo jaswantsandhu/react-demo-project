9f34edf34d876c2c368b27d2f3073d4becabe6a4
Jaswant <jaswant.sandhu88@gmail.com>
2021-06-12 11:55:12 +0530

    Added react router with homepage and search.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import './App.css';
 import Header from './components/header';
 import Content from "./components/content"
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 function App() {
 
   return (
-    <main>
-      <Header></Header>
-      <Content></Content>
-    </main>
+    <Router>
+      <main>
+        <Header></Header>
+        <Content></Content>
+      </main>
+    </Router>
   );
 }
 
--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import SearchListing from "../../pages/search-listing"
 
+import { Switch, Route } from "react-router-dom";
+
 export default function Content()
     {
-        return <SearchListing></SearchListing>
+        return <Switch>
+            <Route exact path="/">
+                <div>Home Page</div>
+            </Route>
+            <Route path="/search">
+                <SearchListing></SearchListing>
+            </Route>
+        </Switch>
     }
+
+// 
