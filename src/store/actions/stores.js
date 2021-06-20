d578ea772f7d1922e6348bd7f72926797825dcd3
Jaswant <jaswant.sandhu88@gmail.com>
2021-06-20 12:41:54 +0530

    handle using async await.

--- src/store/actions/stores.js
+++ src/store/actions/stores.js
@@ -2,16 +2,16 @@ import { GET_STORES_ACTION, GET_STORES_FAILURE_ACTION, GET_STORES_SUCCESS_ACTION
 import axios from "axios";
 
 export function getStores(message) {
-    return function(dispatch){
-        dispatch({ type: GET_STORES_ACTION, payload : [] })
-
-        axios.get("http://localhost:3600/stores")
-        .then((response)=>{
-            dispatch({ type: GET_STORES_SUCCESS_ACTION, payload : response.data })
-        })
-        .catch(()=>{
-            dispatch({ type: GET_STORES_FAILURE_ACTION, payload : [] })
-        })
-
+    return async function(dispatch){
+        dispatch({ type: GET_STORES_ACTION })
+        try
+            {
+                const response = await axios.get("http://localhost:3600/stores", { timeout : 3000 });
+                dispatch({ type: GET_STORES_SUCCESS_ACTION, payload : response.data })
+            }
+        catch(error)
+            {
+                dispatch({ type: GET_STORES_FAILURE_ACTION })
+            }
     }
 }
